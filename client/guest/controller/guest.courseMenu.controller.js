b094e0b871d9d6a89a268e61eda507f0e5ae922c
Tirthraj <tirthraj@tirthrajbarot@yahoo.com>
2016-07-16 15:41:37 +0530

    TP

--- client/guest/controller/guest.courseMenu.controller.js
+++ client/guest/controller/guest.courseMenu.controller.js
@@ -1,40 +1,45 @@
 angular.module('collegeApp')
 	.controller('courseMenuCtrl',['$scope','$state','$stateParams','guestFactory',function($scope,$state,$stateParams,guestFactory){
-		
 	$scope.stateChange = function(x){
 		$state.go('courseDetails',{ 'course' : x.course});
 		console.log(x);
 	}
+	activate();
+	function activate(){
+			guestFactory.getAllCourses()
+			.then(function(response){
+				$scope.courses = response.data;
+			},function(error){
+				console.error(error);
+			});
 
 
-activate();
-function activate(){
+			guestFactory.getHods().then(function(response){
+				$scope.hods=response.data;
+				$scope.getHOD = function(y){
+					var x = 0;
+					var z = $scope.hods;
+					for (x = 0; x < $scope.hods.length; x++) {
+						if(y.localeCompare($scope.hods[x].course)) {
+							return ($scope.hods[x].name);
+						}
+					}
+				}
+				$scope.getHODPhoto = function(y){
+					var x = 0;
+					for (x = 0; x < $scope.hods.length; x++) {
+						if(y.localeCompare($scope.hods[x].course)) {
+							return ($scope.hods[x].photo);
+						}
+					}
+				}
 
-	guestFactory.getAllCourses()
-	.then(function(response){
-		$scope.courses = response.data;
-		/*console.log($scope.courses);*/
-	},function(error){
-		console.error(error);
-	});
+			},function(error){
+				console.error(error);
+			});
+		}
 
-	guestFactory.getHods()
-	.then(function(response){
-		$scope.hods=response.data;
-		console.log($scope.hod);
-	},function(error){
-		console.error(error);
-	});
 
-}
-		/*var course = $stateParams.course;
 
-		guestFactory.getCourseDetails(course)
-		.then(function(response){
-			$scope.record = response.data[0];
-			console.log($scope.record);
 
-		},function(error){
-			console.error(error);
-		});*/
-	}])
+}])
--- server/guest/getHods.php
+++ server/guest/getHods.php
@@ -4,7 +4,10 @@ include("config.php");
 $ara = array();
 $result = mysqli_query($con,"SELECT * FROM teachers where `post` = 'H.O.D.' ") or die(mysqli_error($con));
 while($x = mysqli_fetch_assoc($result))
-echo json_encode($x);
-/*array_push($ara,$x);*/
+{
+    array_push($ara,$x);
+}
+
+echo json_encode($ara);
 
 ?>
